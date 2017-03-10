477223788cf702814a5fe78ced793b9678d09f9a
kaybowwow <kbao@stuy.edu>
2017-03-09 19:49:33 -0500

    changed pivot to random index, renamed variables

--- QuickSort.java
+++ QuickSort.java
@@ -1,7 +1,7 @@
 /*Despoina Sparakis
-APCS2 pd1
-HW #15: So So Quick
-2017-03-08
+  APCS2 pd1
+  HW #15: So So Quick
+  2017-03-08
 */
 
 /*****************************************************
@@ -24,7 +24,7 @@ HW #15: So So Quick
     PROTIP: Assume no duplicates during initial development phase.
     Once you have a working implementation, test against arrays 
     with duplicate values, and revise if necessary. (Backup first.)
- ***/
+***/
 
 public class QuickSort 
 {
@@ -70,64 +70,66 @@ public class QuickSort
      * @param d -- array of ints to be sorted in place
      *****************************************************/
     public static void qsort( int[] d ){
-		int q = Mysterion(d, 0, d.length - 1, 0);
-		if(q == 0){
-			qSortHelper(d, 1, d.length-1 );
-		}
-		else if(q== d.length-1){
-			qSortHelper(d, 0, d.length-2);
-		}
-		else{
-			qSortHelper(d, 0, q-1);
-			qSortHelper(d, q+1, d.length-1);
-		}
+	//pick random index as pivot
+	int pivot = (int)(Math.random() * d.length-1);
+	int q = Mysterion(d, 0, d.length - 1, pivot);
+	if(q == 0){
+	    qSortHelper(d, 1, d.length-1 );
+	}
+	else if(q== d.length-1){
+	    qSortHelper(d, 0, d.length-2);
+	}
+	else{
+	    qSortHelper(d, 0, q-1);
+	    qSortHelper(d, q+1, d.length-1);
+	}
     }
 	
-	public static void qSortHelper(int[] d, int start, int end){
-		if(start == end){
-			//?
-		}
-		else{
-			int q = Mysterion(d, start, end, start);
-			if(q==start){
-				qSortHelper(d, start+1, end);
-			}
-			else if(q==end){
-				qSortHelper(d, start, end-1);
-			}
-			else{
-				qSortHelper(d, start, q-1);
-				qSortHelper(d, q+1, end);
-			}
-		}
+    public static void qSortHelper(int[] d, int start, int end){
+	if(start == end){ }
+	else{
+	    //pick random index between start and end as pivot
+	    int pivot = (int)(Math.random() * (end-start)) + start;
+	    int q = Mysterion(d, start, end, pivot);
+	    if(q==start){
+		qSortHelper(d, start+1, end);
+	    }
+	    else if(q==end){
+		qSortHelper(d, start, end-1);
+	    }
+	    else{
+		qSortHelper(d, start, q-1);
+		qSortHelper(d, q+1, end);
+	    }
 	}
+    }
 
     // Thinkers are encouraged to roll their own subroutines.
     // Insert your auxiliary helper methods here.
 
-	
-	public static int Mysterion(int[] arr, int a, int b, int c){
-		int v = arr[c];
-		swap(c, b, arr);
-		int s = a;
-		for(int i = a; i < b; i++){
-			if(arr[i] < v){
-			swap(s, i, arr);
-			s+=1;
-			}
-		}
-		swap(b, s, arr);
-		return s;  
+    //return s, the final resting position of the pivot	
+    public static int Mysterion(int[] arr, int start, int end, int pivot){
+	int v = arr[pivot];
+	swap(pivot, end, arr);
+	int s = start;
+	for(int i = start; i < end; i++){
+	    if(arr[i] < v){
+		swap(s, i, arr);
+		s+=1;
+	    }
 	}
+	swap(end, s, arr);
+	return s;  
+    }
 
     //main method for testing
     public static void main( String[] args ) 
     {
 	
 	/*int[] q = {7, 8, 4, 2, 6, 3, 9, 5};
-	printArr(q);
-	qsort(q);
-	printArr(q);*/
+	  printArr(q);
+	  qsort(q);
+	  printArr(q);*/
 
 	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
 
@@ -144,7 +146,7 @@ public class QuickSort
 	// randomly-generated arrays of n distinct vals
 	int[] arrN = new int[10];
 	for( int i = 0; i < arrN.length; i++ )
-	    arrN[i] = i;
+	arrN[i] = i;
        
 	System.out.println("\narrN init'd to: " );
 	printArr(arrN);
@@ -159,6 +161,7 @@ public class QuickSort
 	
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
 
 	
 	//get-it-up-and-running, static test case w/ dupes:
@@ -186,9 +189,8 @@ public class QuickSort
 	qsort( arrMatey );
 	System.out.println("arrMatey after sort: " );
 	printArr(arrMatey);
-	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
 
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     }//end main
 
