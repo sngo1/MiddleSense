08ce033ea5e39adb55a7408f2f0f586ef69f297f
alien981 <dsparakis@stuy.edu>
2017-03-09 23:07:26 -0500

    added test results

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -6,23 +6,30 @@ public class QuickSortTester{
 
     public static void main (String[] args) {
 	// arrays of randomly generated ints
-	int[] arr1 = QuickSort.buildArray(1,100);
-   	//int[] arr2 = QuickSort.buildArray(10,100);
-	//int[] arr3 = QuickSort.buildArray(100,100);
-	//int[] arr4 = QuickSort.buildArray(1000,100);
-	//int[] arr5 = QuickSort.buildArray(10000,100);
-	//int[] arr6 = QuickSort.buildArray(100000,100);
+	for(int x = 0; x < 5; x++){
+//	int[] arr1 = QuickSort.buildArray(1,100);
+  // 	int[] arr1 = QuickSort.buildArray(10,100);
+	//int[] arr1 = QuickSort.buildArray(100,100);
+	//int[] arr1 = QuickSort.buildArray(1000,100);
+	int[] arr1 = QuickSort.buildArray(10000,100);
+	//int[] arr1 = QuickSort.buildArray(100000,100);
 
 	//System.out.println("\narr init'd to: " );
-	//QuickSort.printArr(arr6);
+
 	
        	QuickSort.shuffle(arr1);
        	//System.out.println("arr post-shuffle: " );
 	//QuickSort.printArr(arr6);
-
+	double q = System.currentTimeMillis();
 	QuickSort.qsort( arr1 );
+	double y =  System.currentTimeMillis();
+	System.out.println(x + ": " + (y-q));
 	//System.out.println("arr after sort: " );
 	//QuickSort.printArr(arr6);
+	}
+	
+	
+
     }
 }
 
